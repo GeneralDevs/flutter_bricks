81e032ead8dfeaa99dc5f2760dafd0f7975f3f28
Gabriel Donato <gabrielcdonato@gmail.com>
2024-05-23 20:32:03 -0300

    feat: improve repository imports

---
+++ bricks/feature_dartz/__brick__/features/domain/repository/{{feature.snakeCase()}}/{{feature.snakeCase()}}_repository.dart
@@ -0,0 +1,4 @@
+abstract interface class {{feature.pascalCase()}}Repository {
+  Future<Either<Failure, dynamic>> call();
+}
+
--- bricks/feature_dartz/__brick__/features/domain/usecases/{{feature.snakeCase()}}/{{feature.snakeCase()}}_usecase_impl.dart
+++ bricks/feature_dartz/__brick__/features/domain/usecases/{{feature.snakeCase()}}/{{feature.snakeCase()}}_usecase_impl.dart
@@ -1,8 +1,10 @@
+import '{{feature.snakeCase()}}_usecase.dart';
+import '../../repository/{{feature.snakeCase()}}/{{feature.snakeCase()}}_repository.dart';
 
 final class {{feature.pascalCase()}}UsecaseImpl implements {{feature.pascalCase()}}Usecase {
   const {{feature.pascalCase()}}UsecaseImpl({
-    required {{feature.pascalCase()}}Repository 
-  })
+    required {{feature.pascalCase()}}Repository {{feature.camelCase()}}Repository
+  }) : _{{feature.camelCase()}}Repository = {{feature.camelCase()}}Repository;
 
 
   final {{feature.pascalCase()}}Repository _{{feature.camelCase()}}Repository;
