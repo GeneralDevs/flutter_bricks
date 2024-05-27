fa26763f89f2942de347f91754e3e9d405624d20
Gabriel Donato <gabrielcdonato@gmail.com>
2024-05-27 17:01:14 -0300

    improve code

--- generated/lib/entidade.dart
+++ generated/lib/entidade.dart
@@ -1,8 +1,9 @@
 import 'package:equatable/equatable.dart';
 import 'package:testando_poha/user.dart';
 
-class Entidade extends Equatable {
-  const Entidade({
+
+base class Entidade extends Equatable {
+  const Entidade({ 
     this.firstName,
     this.lastName,
     required this.age,
@@ -11,22 +12,9 @@ class Entidade extends Equatable {
     required this.nicknames,
     required this.countriesVisited,
     required this.friends,
-    required this.teste1,
-    required this.teste2,
-    required this.teste3,
-    required this.teste4,
-    required this.teste5,
-    required this.teste6,
-    required this.teste7,
-    required this.teste8,
-    required this.teste9,
-    required this.teste10,
-    required this.teste11,
-    required this.teste12,
-    required this.teste13,
-    required this.teste14,
     required this.romeu,
   });
+
 
   final String? firstName;
 
@@ -44,38 +32,10 @@ class Entidade extends Equatable {
 
   final List<User> friends;
 
-  final String teste1;
-
-  final String teste2;
-
-  final String teste3;
-
-  final String teste4;
-
-  final String teste5;
-
-  final String teste6;
-
-  final String teste7;
-
-  final String teste8;
-
-  final String teste9;
-
-  final String teste10;
-
-  final String teste11;
-
-  final String teste12;
-
-  final String teste13;
-
-  final String teste14;
-
   final int romeu;
 
-  /// Creates a copy of the current Entidade with property changes
-  Entidade copyWith({
+    /// Creates a copy of the current Entidade with property changes
+  Entidade copyWith({ 
     String? Function()? firstName,
     String? Function()? lastName,
     int? age,
@@ -84,20 +44,6 @@ class Entidade extends Equatable {
     List<String>? nicknames,
     List<String?>? countriesVisited,
     List<User>? friends,
-    String? teste1,
-    String? teste2,
-    String? teste3,
-    String? teste4,
-    String? teste5,
-    String? teste6,
-    String? teste7,
-    String? teste8,
-    String? teste9,
-    String? teste10,
-    String? teste11,
-    String? teste12,
-    String? teste13,
-    String? teste14,
     int? romeu,
   }) {
     return Entidade(
@@ -109,25 +55,12 @@ class Entidade extends Equatable {
       nicknames: nicknames ?? this.nicknames,
       countriesVisited: countriesVisited ?? this.countriesVisited,
       friends: friends ?? this.friends,
-      teste1: teste1 ?? this.teste1,
-      teste2: teste2 ?? this.teste2,
-      teste3: teste3 ?? this.teste3,
-      teste4: teste4 ?? this.teste4,
-      teste5: teste5 ?? this.teste5,
-      teste6: teste6 ?? this.teste6,
-      teste7: teste7 ?? this.teste7,
-      teste8: teste8 ?? this.teste8,
-      teste9: teste9 ?? this.teste9,
-      teste10: teste10 ?? this.teste10,
-      teste11: teste11 ?? this.teste11,
-      teste12: teste12 ?? this.teste12,
-      teste13: teste13 ?? this.teste13,
-      teste14: teste14 ?? this.teste14,
       romeu: romeu ?? this.romeu,
     );
   }
 
-  @override
+
+    @override
   List<Object?> get props => [
         firstName,
         lastName,
@@ -137,20 +70,8 @@ class Entidade extends Equatable {
         nicknames,
         countriesVisited,
         friends,
-        teste1,
-        teste2,
-        teste3,
-        teste4,
-        teste5,
-        teste6,
-        teste7,
-        teste8,
-        teste9,
-        teste10,
-        teste11,
-        teste12,
-        teste13,
-        teste14,
         romeu,
       ];
+
+
 }
