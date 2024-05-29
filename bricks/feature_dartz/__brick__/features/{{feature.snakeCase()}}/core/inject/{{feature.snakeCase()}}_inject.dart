08c78623a237ad14dcd7ea3863417bdb5a0ea460
Gabriel Donato <gabrielcdonato@gmail.com>
2024-05-29 10:56:57 -0300

    inject file

--- bricks/feature_dartz/__brick__/features/{{feature.snakeCase()}}/core/inject/{{feature.snakeCase()}}_inject.dart
+++ bricks/feature_dartz/__brick__/features/{{feature.snakeCase()}}/core/inject/{{feature.snakeCase()}}_inject.dart
@@ -1,4 +1,21 @@
 sealed class {{feature.pascalCase()}}Inject {
+  static void inject(GetIt getIt) {
+    getIt.registerFactory<{{feature.pascalCase()}}Usecase>(
+      () => {{feature.pascalCase()}}UsecaseImpl(
+        {{feature.camelCase()}}Repository: getIt<{{feature.pascalCase()}}Repository>(),
+      ),
+    );
 
+    getIt.registerFactory<{{feature.pascalCase()}}Repository>(
+      () => {{feature.pascalCase()}}RepositoryImpl(
+        {{feature.camelCase()}}Datasource: getIt<{{feature.pascalCase()}}Datasource>(),
+      ),
+    );
 
+    getIt.registerFactory<{{feature.pascalCase()}}Datasource>(
+      () => {{feature.pascalCase()}}DatasourceImpl(
+        restClientPost: getIt<RestClientPost>(),
+      ),
+    );
+  }
 }
